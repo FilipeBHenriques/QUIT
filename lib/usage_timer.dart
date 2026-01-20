380f687d93b42a9259a01dcf33035237087d1bf6
FilipeYelhow <filipebh@yelhow.com>
2026-01-20 00:09:01 +0000

    24 hours reset

--- lib/usage_timer.dart
+++ lib/usage_timer.dart
@@ -17,7 +17,7 @@ class UsageTimer {
 
   UsageTimer(
     this._prefs, {
-    this.resetInterval = const Duration(hours: 0, minutes: 0, seconds: 30),
+    this.resetInterval = const Duration(hours: 24, minutes: 0, seconds: 0),
   }) {
     // Save reset interval to preferences so MonitoringService can read it
     _prefs.setInt('reset_interval_seconds', resetInterval.inSeconds);
